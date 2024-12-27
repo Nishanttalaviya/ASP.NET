7b0c4abf04be97307c54df2fa7c246121ac136ee
nishant talaviya <nishanttalaviya2308@gmail.com>
2024-12-27 20:17:46 +0530

    commit code

---
+++ ASP.NET/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ASP.NET.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <asp:Label ID="Label1" runat="server" Text="sum1:"></asp:Label>
+
+        <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="sum 2:"></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="add" />
+        
+        <br />
+        
+    </form>
+    
+</body>
+</html>
---
+++ ASSIGNMENT/ASSIGNMENT/Default.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Assignments
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void BtnWelcome_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text.Trim();
+            lblWelcomeMessage.Text = !string.IsNullOrEmpty(name) ? $"Welcome, {name}!" : "Please enter your name.";
+        }
+
+        protected void BtnConvert_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtTemperature.Text.Trim(), out double celsius))
+            {
+                double fahrenheit = (celsius * 9 / 5) + 32;
+                lblTemperatureResult.Text = $"Temperature in Fahrenheit: {fahrenheit:F2}°F";
+            }
+            else
+            {
+                lblTemperatureResult.Text = "Please enter a valid temperature in Celsius.";
+            }
+        }
+
+        protected void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtNumber1.Text.Trim(), out double num1) && double.TryParse(txtNumber2.Text.Trim(), out double num2))
+            {
+                string operation = ddlOperation.SelectedValue;
+                double result = 0;
+
+                if (operation == "+")
+                {
+                    result = num1 + num2;
+                }
+                else if (operation == "-")
+                {
+                    result = num1 - num2;
+                }
+                else if (operation == "*")
+                {
+                    result = num1 * num2;
+                }
+                else if (operation == "/")
+                {
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        lblCalculationResult.Text = "Division by zero is not allowed.";
+                        return;
+                    }
+                }
+                else
+                {
+                    lblCalculationResult.Text = "Invalid operation selected.";
+                    return;
+                }
+
+                lblCalculationResult.Text = $"Result: {result:F2}";
+            }
+            else
+            {
+                lblCalculationResult.Text = "Please enter valid numbers.";
+            }
+        }
+
+        protected void DdlColors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedColor = ddlColors.SelectedValue;
+            lblColorMessage.Text = !string.IsNullOrEmpty(selectedColor)
+                ? $"You selected: {selectedColor}"
+                : "Please select a color.";
+            lblColorMessage.Style["color"] = selectedColor.ToLower();
+        }
+    }
+}
